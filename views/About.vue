f3a6f790cc37263e48483f34952eed13597c3198
minimallinux <anthonymcgowan1@gmail.com>
2018-06-03 19:04:21 +0100

    Update

---
+++ views/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+   <h2>{{ msg }}</h2>
+    <p>
+      For other projects and information check out our
+      <a href="https://webmobapps.com" target="_blank"> Other Site</a>.
+    </p> 
+  </div>
+</template>
+<script>
+import App from '../App'
+
+export default {
+  name: 'about',
+  components: {
+    App
+  },
+  data() {
+    return { msg: 'This is an about page!' }
+  }
+  }
+</script>
+
+
