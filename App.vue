3d88bf83cbda0256628863d70839a9dd5c40c23f
minimallinux <anthonymcgowan1@gmail.com>
2018-06-03 20:33:36 +0100

    Add some basic styles

--- App.vue
+++ App.vue
@@ -9,3 +9,36 @@
     <router-view/>
   </div>
 </template>
+<style>
+body {
+font-family: 'Verdana', sans-serif;
+ display:grid;
+ grid-template-rows:auto;
+ justify-items:center;
+padding-top:30px;
+}
+body, html {
+margin:0;
+height:100%;
+}
+#app {
+width:90%;
+}
+#nav {
+padding:10px 10px 10px 0;
+}
+#nav a {
+padding:10px;
+font-size: 1.3em;
+border-radius:5px;
+background-color:#e0edf4;
+text-decoration:none;
+color:#3e5252;
+}
+h2 {
+font-size: 1.4em;
+}
+.center {
+text-align:center;
+}
+</style>
